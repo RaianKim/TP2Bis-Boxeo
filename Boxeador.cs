e072e92974754793a1cb1390984bd099f2d47cc7
RaianKim <raiank2007@gmail.com>
2023-04-14 02:07:19 -0300

    Agregamos Funciones.cs y Boxeador.cs

---
+++ Boxeador.cs
@@ -0,0 +1,15 @@
+using System;
+class Boxeador
+{
+    // Definicion de Propiedades
+    public int DNI{get;private set;}
+    public string Apellido{get;set;}
+    public string Nombre{get;set;}
+    public DateTime FechaNacimiento{get;set;}
+    public string Email{get;set;}
+// Constructor
+public Boxeador()
+{
+
+}
+}
