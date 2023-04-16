041480ecf721370a3c5a7f57bb7e1721afa5dcd4
RaianKim <raiank2007@gmail.com>
2023-04-16 02:05:34 -0300

    Agregamos la funcion de Pelea

--- Boxeador.cs
+++ Boxeador.cs
@@ -2,11 +2,11 @@ using System;
 class Boxeador
 {
     // Definicion de Propiedades
-    public string Nombre{ get;set;}
+    public string Nombre{get;set;}
     public string Pais{get;set;}
-    public int peso{get;set;}
-    public int PotenciaGolpes{get; private set;}
-    public int VelocidadPiernas{get; private set;}
+    public int Peso{get;set;}
+    public int PotenciaGolpes{get; set;}
+    public int VelocidadPiernas{get; set;}
     
     
 // Constructor
@@ -14,11 +14,18 @@ public Boxeador()
 {
 
 }
-
-public double ObtenerSkill(int VeloPi, int  PotenaGol)
+public Boxeador(string nom, string pais, int peso, int potgol, int velopi)
 {
-    double a = VeloPi * 0.6;
-    double b = PotenaGol * 0.8;
+    Nombre = nom;
+    Pais = pais;
+    Peso = peso;
+    PotenciaGolpes = potgol;
+    VelocidadPiernas = velopi;
+}
+public double ObtenerSkill()
+{
+    double a = PotenciaGolpes * 0.6;
+    double b = VelocidadPiernas * 0.8;
     double c = Funciones.numeroAleatorio(1,11);
     return a+b+c;
 }
--- Program.cs
+++ Program.cs
@@ -1,37 +1,79 @@
-﻿Console.WriteLine("1. Cargar Boxeador N°1");
+﻿Dictionary<string,Boxeador> listBoxeador = new Dictionary<string, Boxeador>();
+string Box1 = "Box1";
+string Box2 = "Box2";
+bool box1 = false ,box2 = false ,pel = false;
+Console.WriteLine("1. Cargar Boxeador N°1");
 Console.WriteLine("2. Cargar Boxeador N°2");
 Console.WriteLine("3. Pelea!!");
 Console.WriteLine("4. Fin");
-int respuesta = Funciones.IngresarEnteroEnRango("Escriba 1,2,3  o 4",1,3);
+int respuesta = Funciones.IngresarEnteroEnRango("Escriba 1,2,3  o 4",1,5);
+
 switch(respuesta)
 {
 
 case 1:
-    ObtenerBox1();
+    if(box1 == true)
+    {
+        Console.WriteLine("No se puede ingresar de nuevo el boxeador N°1");
+    }   
+    else
+    {
+        ObtenerBox1(Box1);
+    }
+    
+    
 break;
     
 case 2:
-    ObtenerBox2();
+    if(box2 == true)
+    {
+        Console.WriteLine("No se puede ingresar de nuevo el boxeador N°2");
+    }   
+    else
+    {
+        ObtenerBox2(Box2);
+    }
 break;
-
+    
 case 3:
-
+    if(box1 == false || box1 == false)
+    {
+        Console.WriteLine("No se puede hacer la pelea. Ingrese de acordar los datos de los dos boxeadores");
+    } 
+    else
+    {
+        Pelea(Box1,Box2);
+    }
+    
 break;
 
 
 }
 
-void ObtenerBox1(){
+void ObtenerBox1(string Box1){
 string nombre = Funciones.IngresarTexto("Ingrese el nombre del boxeador"); 
 string pais = Funciones.IngresarTexto("Ingresar el nombre del pais donde nacio");
 int peso = Funciones.IngresarEntero("Ingresar el peso");
 int potgol = Funciones.numeroAleatorio(1,101);
 int velopi = Funciones.numeroAleatorio(1,101);
+Boxeador boxeador1 = new Boxeador(nombre,pais,peso,potgol,velopi);
+listBoxeador.Add(Box1,boxeador1);
+ Console.WriteLine($"Se ha creado la persona {nombre} y se ha agregado como el Boxeador N°1.");
 }
-void ObtenerBox2(){
+void ObtenerBox2(string Box2){
 string nombre = Funciones.IngresarTexto("Ingrese el nombre del boxeador"); 
 string pais = Funciones.IngresarTexto("Ingresar el nombre del pais donde nacio");
 int peso = Funciones.IngresarEntero("Ingresar el peso");
 int potgol = Funciones.numeroAleatorio(1,101);
 int velopi = Funciones.numeroAleatorio(1,101);
+Boxeador boxeador2 = new Boxeador(nombre,pais,peso,potgol,velopi);
+listBoxeador.Add(Box2,boxeador2);
+ Console.WriteLine($"Se ha creado la persona {nombre} y se ha agregado como el Boxeador N°2.");
+}
+string Pelea(string Box1, string Box2){
+    string ganador = "";
+    double boxer1,boxer2;
+    boxer1 = listBoxeador[Box1].ObtenerSkill();
+    boxer2 = listBoxeador[Box1].ObtenerSkill();
+    return ganador;
 }
